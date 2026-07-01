dfa9be95aef121764f3c35a2603636071076d1db
henrikts99 <henriko99@live.no>
2026-07-01 16:41:42 +0200

    add views router and dashboard page for showing buyorder states with info. Setup Pico CSS for quick styling

--- app/routers/views.py
+++ app/routers/views.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.crud import get_stored_buyorder_states
+from app.db.base import get_db
 
 router = APIRouter()
 
@@ -9,5 +13,10 @@ templates = Jinja2Templates(directory="app/templates")
 
 
 @router.get("/", response_class=HTMLResponse)
-async def display_dashboard(request: Request):
-    return templates.TemplateResponse(request=request, name="pages/dashboard.html")
+async def display_dashboard(request: Request, db: AsyncSession = Depends(get_db)):
+    beaten_buyorders = await get_stored_buyorder_states(db, only_beaten=False)
+    return templates.TemplateResponse(
+        request=request,
+        name="pages/dashboard.html",
+        context={"beaten_buyorders": beaten_buyorders},
+    )
