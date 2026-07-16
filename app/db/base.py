58be9c46654f69ea2fb054e70ffe7c6a505f6233
henrikts99 <henriko99@live.no>
2026-07-16 13:32:30 +0200

    enable SQLite WAL mode for concurrent access
    This is done to fix issue where you cannot update a buyorder when a
    "sync and scan" is happening.

--- app/db/base.py
+++ app/db/base.py
@@ -1,9 +1,19 @@
+from sqlalchemy import event
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
 DATABASE_URL = "sqlite+aiosqlite:///./trading.db"
 
 engine = create_async_engine(DATABASE_URL)
+
+
+# function called every time a new DB connection is opened
+@event.listens_for(engine.sync_engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA journal_mode=WAL")
+    cursor.close()
+
 
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
